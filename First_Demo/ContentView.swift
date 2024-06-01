57b4b8bc9e13f761be75896a12aa78e972620708
LearnSwiftProgramming <beadeveloper@gmail.com>
2024-06-01 16:50:21 +0530

    Initial Commit

---
+++ First_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First_Demo
+//
+//  Created by Rahul Ajmera on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First_Demo/First_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_DemoApp.swift
+//  First_Demo
+//
+//  Created by Rahul Ajmera on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct First_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
